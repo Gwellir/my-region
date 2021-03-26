916d77b964ff7117783c44c73c3839627b2e81ff
Vlad Chekunov <gwellir@gmail.com>
2021-03-26 18:24:47 +0300

    tune admin to use RoutePhoto

--- travelapp/admin.py
+++ travelapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from travelapp.models import Route, Trip, TripOptionAvailable, Region, District
+from travelapp.models import Route, RoutePhoto, Trip, TripOptionAvailable, Region, District
 
 admin.site.register(Route)
+admin.site.register(RoutePhoto)
 admin.site.register(Trip)
 admin.site.register(TripOptionAvailable)
 admin.site.register(Region)
