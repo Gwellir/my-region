061c587d4694e6b1b45950d96a0ec47b7d9f20ea
Vlad Chekunov <gwellir@gmail.com>
2021-04-29 17:16:43 +0300

    add pytest add a trivial pytest task

--- travelapp/tests.py
+++ travelapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+# View tests
+class TravelViewTests(TestCase):
+    def test_main(self):
+        """
+        Tests whether main page is available.
+        """
+
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
